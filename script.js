7ee2bc5adf5793b73af0096e85094003872cfd33
Dimola Ogidan <didiogidan@gmail.com>
2020-05-12 09:29:22 +0100

    created a function to add new items to the list

---
+++ script.js
@@ -0,0 +1,24 @@
+const addTodo = document.querySelector(".add");
+const list = document.querySelector(".todos");
+
+const generateTemplate = (todo) => {
+  const html = `
+      <li
+        class="list-group-item d-flex justify-content-between align-itms-center">
+          <span>${todo}</span>
+          <i class="far fa-trash-alt delete"></i>
+      </li>
+   `;
+
+  list.innerHTML += html;
+};
+
+addTodo.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = addTodo.add.value.trim();
+
+  if (todo.length) {
+    generateTemplate(todo);
+    addTodo.reset();
+  }
+});
