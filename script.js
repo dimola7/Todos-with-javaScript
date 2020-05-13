aa8454f3ce130b419871b9e609d181afe607771d
Dimola Ogidan <didiogidan@gmail.com>
2020-05-13 10:16:23 +0100

    added search functionality

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const addTodo = document.querySelector(".add");
 const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
 
 const generateTemplate = (todo) => {
   const html = `
-      <li
-        class="list-group-item d-flex justify-content-between align-itms-center">
+      <li class="list-group-item d-flex justify-content-between align-itms-center">
           <span>${todo}</span>
           <i class="far fa-trash-alt delete"></i>
       </li>
@@ -28,4 +28,19 @@ list.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove();
   }
+});
+
+const filterTodos = (term) => {
+  Array.from(list.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add("filtered"));
+
+  Array.from(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove("filtered"));
+};
+
+search.addEventListener("keyup", () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
 });
